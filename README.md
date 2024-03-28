98824db6fe2cb81e5bb0a15ac0b8d165b3a76146
steven.liatti <steven@liatti.ch>
2024-03-28 10:47:30 +0100

    Sort files, refactoring (with enum) and improve management when multiples barcodes found

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Renamator
 
-Find barcode in image file with [opencv](https://pypi.org/project/opencv-python/) [pyzbar](https://pypi.org/project/pyzbar/) libraries and rename it to the barcode value.
+Find barcode in image file with [opencv](https://pypi.org/project/opencv-python/) and [pyzbar](https://pypi.org/project/pyzbar/) libraries and rename it to the barcode value.
 
+Coded with Python 3.12.2.
+
+Run like this :
+
+`python3 renamator.py <images_dir> <extension> <results_file>`
--- renamator.py
+++ renamator.py
@@ -4,46 +4,66 @@ import os
 import sys
 import cv2
 from pyzbar.pyzbar import decode  # type: ignore
+from enum import Enum
 
 # mypy imports
 from pyzbar.pyzbar import Decoded  # type: ignore
 from typing import List
+from typing import Tuple
 
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        print("You need to give a correct images directory and files extension, ex: ")
-        print("python3 renamator.py /tmp/images tif")
+    if len(sys.argv) < 3:
+        print("To run : python3 renamator.py <images_dir> <extension> <results_file>, ex: ")
+        print("python3 renamator.py /tmp/images tif /tmp/res.csv")
     else:
         images_dir: str = sys.argv[1]
         extension: str = sys.argv[2]
-        os.chdir(images_dir)
+        results_file: str = sys.argv[3]
 
-        images: List[str] = [file for file in os.listdir(
-            images_dir) if file.lower().endswith(f".{extension}")]
+        images: List[str] = [file for file in sorted(os.listdir(
+            images_dir)) if file.lower().endswith(f".{extension}")]
 
+        results = []
         for image in images:
-            # if "_" not in image:
             print(f"Process {image}:")
             image_path: str = os.path.join(images_dir, image)
-            find_barcodes_and_rename_file(image_path)
-            print()
+            res, new_image_path = find_barcodes_and_rename_file(image_path)
+            results.append(f"{str(res)}\t{image_path}\t{new_image_path}")
+            match res:
+                case DecodingResult.UNREADABLE:
+                    print(f"Unable to read barcode in the image '{
+                          image_path}'\n")
+                case DecodingResult.UNIQUE:
+                    print(f"Code found and image '{
+                          image_path}' renamed to '{new_image_path}'\n")
+                case DecodingResult.MULTIPLE:
+                    print(f"Multiple barcodes found: {new_image_path}\n")
+
+        with open(results_file, "w") as file:
+            file.write("\n".join(results))
 
 
-def find_barcodes_and_rename_file(image_path: str) -> None:
+DecodingResult = Enum("DecodingResult", ["UNREADABLE", "UNIQUE", "MULTIPLE"])
+
+
+def find_barcodes_and_rename_file(image_path: str) -> Tuple[DecodingResult, str]:
     barcodes = find_barcodes(image_path)
     if len(barcodes) == 0:
-        print(f"Unable to read barcode in the image '{image_path}'")
+        return DecodingResult.UNREADABLE, ""
     elif len(barcodes) == 1:
-        barcode = barcodes[0].data.decode("utf-8")
-        new_image_path = make_new_name(image_path, barcode)
-        compare_names(image_path, barcode)
-        # os.rename(image_path, new_image_path)
-        print(
-            f"Code found and image renamed '{image_path}' to '{new_image_path}'")
+        new_image_path = rename_file(
+            image_path, barcodes[0].data.decode("utf-8"))
+        return DecodingResult.UNIQUE, new_image_path
     else:
-        to_print = [b.data for b in barcodes if b.type == "CODE128"]
-        print(f"Multiple barcodes found: {to_print}")
+        barcodes_data = [b.data for b in barcodes if b.type ==
+                         "CODE128" and len(b.data) != 0]
+        if len(barcodes_data) == 1:
+            new_image_path = rename_file(
+                image_path, barcodes_data[0].decode("utf-8"))
+            return DecodingResult.UNIQUE, new_image_path
+        else:
+            return DecodingResult.MULTIPLE, str(barcodes_data)
 
 
 def find_barcodes(image_path: str) -> List[Decoded]:
@@ -52,10 +72,16 @@ def find_barcodes(image_path: str) -> List[Decoded]:
     # Convert color image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply binary threshold
-    _, ad_thr = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
     # Find barcodes in image
-    barcodes = decode(ad_thr)
+    barcodes: List[Decoded] = decode(binary_image)
     return barcodes
+
+
+def rename_file(image_path: str, barcode: str) -> str:
+    new_image_path = make_new_name(image_path, barcode)
+    os.rename(image_path, new_image_path)
+    return new_image_path
 
 
 def make_new_name(image_path: str, barcode: str) -> str:
@@ -63,13 +89,6 @@ def make_new_name(image_path: str, barcode: str) -> str:
     new_image_name = f"{barcode}{ext}"
     new_image_path = os.path.join(os.path.dirname(image_path), new_image_name)
     return new_image_path
-
-
-def compare_names(image_path: str, barcode: str) -> None:
-    hierarchy, file = os.path.split(image_path)
-    file_name, _ = os.path.splitext(file)
-    if file_name != barcode:
-        print(f"Image name: {file_name}, barcode value: {barcode}")
 
 
 if __name__ == "__main__":
